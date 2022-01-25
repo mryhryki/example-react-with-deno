01024ce6b61d038bd3c762763eec9f77ab7b56a4
mryhryki <mryhryki@gmail.com>
2022-01-25 19:25:31 +0900

    Build with React.createElement()

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
-// import React from "https://esm.sh/react";
-import ReactDOM from "https://esm.sh/react-dom";
+import React from "https://esm.sh/react";
+// import ReactDOM from "https://esm.sh/react-dom";
 // import { Index } from "./index.js";
 //
 // ReactDOM.render(<Index />, document.getElementById("react-root"));
 
-console.log("BUILD")
+console.log(React.createElement("div", {}, "Hello World"));
