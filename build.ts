a5bbf3ff10cec6410d2df24a9a90ecc87824dee1
mryhryki <mryhryki@gmail.com>
2022-02-07 10:09:01 +0900

    Use esbuild.build()

--- build.ts
+++ build.ts
@@ -1,27 +1,37 @@
 import * as esbuild from "https://deno.land/x/esbuild@v0.14.13/mod.js";
 
-const { files } = await Deno.emit("./react/app.tsx", {
-  bundle: "module",
-  check: true,
+// const { files } = await Deno.emit("./react/App.tsx", {
+//   bundle: "module",
+//   check: true,
+// });
+//
+// const result = await esbuild.transform(
+//   files["deno:///bundle.js"],
+//   {
+//     minify: true,
+//     sourcemap: true,
+//     define: {
+//       "ENVIRONMENT": '"production"',
+//     },
+//     format: "esm",
+//   },
+// );
+// const { warnings, code, map } = result;
+// if (warnings.length > 0) {
+//   warnings.forEach((warnings) => console.warn(warnings));
+// }
+//
+// const encoder = new TextEncoder();
+// await Deno.writeFile("./public/bundle.js", encoder.encode(code));
+// await Deno.writeFile("./public/bundle.js.map", encoder.encode(map));
+
+await esbuild.build({
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  format: "esm",
+  entryPoints: ["./react/App.tsx"],
+  outfile: "./public/bundle.js",
 });
 
-const result = await esbuild.transform(
-  files["deno:///bundle.js"],
-  {
-    minify: true,
-    sourcemap: true,
-    define: {
-      "ENVIRONMENT": '"production"',
-    },
-    format: "esm",
-  },
-);
-const { warnings, code, map } = result;
-if (warnings.length > 0) {
-  warnings.forEach((warnings) => console.warn(warnings));
-}
-
-const encoder = new TextEncoder();
-await Deno.writeFile("./public/bundle.js", encoder.encode(code));
-await Deno.writeFile("./public/bundle.js.map", encoder.encode(map));
 esbuild.stop();
