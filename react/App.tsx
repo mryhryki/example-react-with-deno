daf84dafd295b7de1bd7d8abad7bdb5d92a0e8a0
mryhryki <mryhryki@gmail.com>
2022-02-13 02:59:02 +0900

    Remove global style

--- react/App.tsx
+++ react/App.tsx
@@ -1,20 +1,12 @@
 import React from "https://cdn.skypack.dev/react@17.0.2?dts";
 import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.2?dts";
 import { Route, Switch } from "https://cdn.skypack.dev/wouter@2.7.5?dts";
-import { createGlobalStyle } from "https://cdn.skypack.dev/styled-components@5.3.3?dts";
 import { Home } from "./pages/Home.tsx";
 import { Counter } from "./pages/Counter.tsx";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-  }
-`;
 
 const App = () => {
   return (
     <React.Fragment>
-      <GlobalStyle />
       <Switch>
         <Route path="/counter">
           <Counter />
